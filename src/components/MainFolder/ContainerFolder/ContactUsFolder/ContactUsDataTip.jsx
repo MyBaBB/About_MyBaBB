bff3d3c4bb57a39d00def96756462752f7bf60f3
Brett Baker <GitBaBB@MyBaBB.com>
2024-04-16 03:46:56 -0700

    npm run format

--- src/components/MainFolder/ContainerFolder/ContactUsFolder/ContactUsDataTip.jsx
+++ src/components/MainFolder/ContainerFolder/ContactUsFolder/ContactUsDataTip.jsx
@@ -1,22 +1,19 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ContactsUs from './ContactUs'
-import './ContactUs.css'
+import React from "react";
+import ContactsUs from "./ContactUs";
+import "./ContactUs.css";
 const ContactUsDataTip = () => {
   return (
     <div>
-       <div className="">
+      <div className="">
         <a className="" href="https://MyBabb.com">
-          <div
-            className="dataToolTip14"
-            data-tool-tip="My Main Page"
-          >
+          <div className="dataToolTip14" data-tool-tip="My Main Page">
             <ContactsUs />
           </div>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUsDataTip
+export default ContactUsDataTip;
